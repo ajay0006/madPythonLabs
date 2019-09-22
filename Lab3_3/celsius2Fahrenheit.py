2b5c561d0c426506cd1487ed858e23638e0fd1ac
ajay0006 <ajay0006@algonquinlive.com>
2019-09-22 11:37:29 -0400

    by master

--- Lab3_3/celsius2Fahrenheit.py
+++ Lab3_3/celsius2Fahrenheit.py
@@ -1,1 +1,9 @@
-print("Hello world")
+# Celsius to Fahrenheit conversion
+celsius = input("Enter temp in celsius: ")
+
+fahrenheit = float(float(celsius) * (9 / 5)) + 32
+
+print("Your Temperature is: " + str(fahrenheit) + '\u1D3C' "F")
+
+# kelvin = (fahrenheit + 459.67) * (5 / 9)
+# print("Your Temperature is:", kelvin, "Kelvin")
