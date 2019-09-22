6d53052aad7462ed41bbafca2aee3c8142069b06
ajay0006 <ajay0006@algonquinlive.com>
2019-09-22 04:31:42 -0400

    by master

---
+++ Lab3_1/areaOfCircle.py
@@ -0,0 +1,9 @@
+# Calculating Area of a Circle
+Pi = 22/7
+print(Pi)
+print(type(Pi))
+radius = input("Please enter the radius: ")
+print(type(radius))
+Area = Pi * (int(radius)) ** 2
+print(type(Area))
+print("The Area of your Circle is:", Area)
