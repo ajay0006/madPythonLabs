db506359ca3a2a04e911b2f44597b14f7eabeea3
ajay0006 <55412457+ajay0006@users.noreply.github.com>
2019-11-20 13:43:02 -0500

    completed Task 1 of the assignment

---
+++ Lab 11/practice.py
@@ -0,0 +1,14 @@
+from modules import convert_to_dict as f
+
+
+getList = f('presidents.csv')
+
+index = []
+name = []
+for row in getList:
+    index.append(row['Presidency'])
+    name.append(row['President'])
+print(index, 'first')
+print(name, 'second')
+combo = zip(index, name)
+print(combo.__str__())
--- Lab 11/presidents.py
+++ Lab 11/presidents.py
@@ -9,12 +9,31 @@ print(presidents_List)
 
 @app.route('/')
 def index():
-    heading = '<h1>Welcome to The Presidential Flask Example!</h1>'
-    test1 = '<p>' + presidents_List[0]['President']
-    test2 = ", born in " + presidents_List[0]['Birthplace'] + '.<p>'
-    return heading + test1 + test2
+    # heading = '<h1>Welcome to The Presidential Flask Example!</h1>'
+    # test1 = '<p>' + presidents_List[0]['President']
+    # test2 = ", born in " + presidents_List[0]['Birthplace'] + '.<p>'
+    president_Ids = []
+    presidentNames = []
+    for presido in presidents_List:
+        president_Ids.append(presido['Presidency'])
+        presidentNames.append(presido['President'])
+    pairs_list = zip(president_Ids, presidentNames)
+    return render_template('index.html', pairs=pairs_list, the_title="Presidents Index")
+    # return heading + test1 + test2
     # return '<h1>Welcome to the presidential Flask example!</h1>'
 
+
+@app.route('/president/<num>')
+def detail(num):
+    for president in presidents_List:
+        if president['Presidency'] == num:
+            pres_dict = president
+            break
+    return render_template('president.html', pres=pres_dict, the_title=pres_dict['President'])
+
+
+# test = index()
+# print(test[2])
 
 # your code here
 
